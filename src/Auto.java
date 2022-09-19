57de3f1aec05d7a4561505280f0d93165ca28d0b
dynastyo <030tee@gmail.com>
2022-09-19 09:45:55 +0200

    v0.1

---
+++ src/Auto.java
@@ -0,0 +1,80 @@
+public class Auto {
+private String brand, id;
+//model, id
+//private double value;
+//private int topSpeed;
+//private boolean used;
+
+    public Auto() {
+    }
+
+//    public Auto(String brand, String model, String id, boolean used) {
+//        this.brand = brand;
+//        this.model = model;
+//        this.id = id;
+//        this.used = used;
+//    }
+    public Auto(String id, String brand) {
+        this.brand = brand;
+        this.id = id;
+    }
+
+    //    public Auto(String brand, String model, String id, double value, int topSpeed, boolean used) {
+//        this.brand = brand;
+//        this.model = model;
+//        this.id = id;
+//        this.value = value;
+//        this.topSpeed = topSpeed;
+//        this.used = used;
+//    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+//    public String getModel() {
+//        return model;
+//    }
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+//    public double getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(double value) {
+//        this.value = value;
+//    }
+//
+//    public int getTopSpeed() {
+//        return topSpeed;
+//    }
+//
+//    public void setTopSpeed(int topSpeed) {
+//        this.topSpeed = topSpeed;
+//    }
+//
+//    public boolean isUsed() {
+//        return used;
+//    }
+//
+//    public void setUsed(boolean used) {
+//        this.used = used;
+//    }
+
+}
---
+++ src/Database.java
@@ -0,0 +1,78 @@
+public class Database {
+    private final Auto[] carpark;
+
+    public Database() {
+        carpark = new Auto[50];
+    }
+
+    // Rückgabe aller Datensätze
+    public void ausgabeAutoliste() {
+
+        for (int i = 0; i < 50; i++) {
+            if (carpark[i] != null) {
+                System.out.println("------------");
+                System.out.println("ID " + carpark[i].getId());
+                System.out.println("Brand " + carpark[i].getBrand());
+            }
+        }
+        System.out.println("------------");
+        System.out.println("Ausgabe Beendet.");
+        System.out.println("Zurück zum Hauptmenü? J/N");
+        String auswahl = Main.sc.next().toUpperCase();
+        if (auswahl.equals("J")) {
+            Main.mainMenu();
+        }
+    }
+public void datensatzLoeschen(){}
+
+    // Hinzufügen eines Autos
+    public void hinzufuegenAuto(Auto a) {
+        for (int i = 0; i < 50; i++) {
+            if (carpark[i] == null) {
+                carpark[i] = a;
+                break;
+            }
+
+        }
+    }
+    public boolean idFree(String id) {
+        for (int i = 0; i < 50; i++) {
+            if (carpark[i] != null) {
+            if (carpark[i].getId().equals(id)) {
+                return false;
+            }}
+        }
+        return true;
+    }
+
+    public void sucheAuto() {
+        System.out.println("suchbegirff sadasdfasd");
+        String suchbegriff = Main.sc.next();
+        for (int i = 0; i < 50; i++) {
+            if (carpark[i] != null) {
+                if (suchbegriff.equals(carpark[i].getBrand()) || suchbegriff.equals(carpark[i].getId())) {
+                    System.out.println("------------");
+                    System.out.println("ID " + carpark[i].getId());
+                    System.out.println("Brand " + carpark[i].getBrand());
+                }
+            }
+        }
+        System.out.println("------------");
+        System.out.println("Ausgabe Beendet.");
+        System.out.println("Zurück zum Hauptmenü? J/N");
+        String auswahl = Main.sc.next().toUpperCase();
+        if (auswahl.equals("J")) {
+            Main.mainMenu();
+        }
+    }
+
+    public void dummyDaten(){
+        carpark[0] = new Auto("1", "BMW");
+        carpark[1] = new Auto("2", "Audi");
+        carpark[2] = new Auto("3", "BMW");
+        carpark[3] = new Auto("4", "VW");
+        carpark[4] = new Auto("5", "Opel");
+    }
+}
+
+
